431a0249bdc16f4b888015d55f471f45b912db2d
fatemeh valinezhad <valinezhadfatemeh@gmail.com>
2025-02-18 15:52:48 +0100

    Fati-Projekt

---
+++ index.js
@@ -0,0 +1,36 @@
+const buttonElement = document.getElementById('read-more');
+const pElement = document.getElementById('more-info');
+const showInfo = (domElement) => {
+    if (domElement.style.display !== 'block') {
+        domElement.style.display = 'block';
+    } else {
+        domElement.style.display = 'none'
+
+    }
+}
+buttonElement.addEventListener('click', () => {
+    showInfo(pElement);
+})
+const menu = document.querySelector(".right-menu");
+const items = document.querySelectorAll(".item");
+const hamburger = document.querySelector(".hamburger");
+const menuIcon = document.querySelector(".menuIcon");
+const closeIcon = document.querySelector(".closeIcon");
+function navmenu() {
+    if (menu.classList.contains("showMenu")) {
+        menu.classList.remove("showMenu");
+        closeIcon.style.display = "none";
+        menuIcon.style.display = "block";
+    } else {
+        menu.classList.add("showMenu");
+        closeIcon.style.display = "block";
+        menuIcon.style.display = "none";
+    }
+}
+hamburger.addEventListener('click', navmenu);
+items.forEach(
+    function (item) {
+        item.addEventListener('click', navmenu);
+    }
+)
+
