344fba42d4d5e5401784b908a5e4f0028e787d42
fatemeh valinezhad <valinezhadfatemeh@gmail.com>
2025-03-12 11:23:51 +0100

    my website

--- index.js
+++ index.js
@@ -34,5 +34,39 @@ items.forEach(
         item.addEventListener('click', navmenu);
     }
 )
-const apikey = "169ea1bcf7343a0cc211782e287a9b74";
+let weather = {
+    apikey: "169ea1bcf7343a0cc211782e287a9b74",
+    fetchweather: function (city) {
+       fetch(
+        "http://api.openweathermap.org/data/2.5/weather?q=" 
+        + city 
+        + "&units=metric&appid="
+        + this.apikey
+        
+       )
+       .then((response) => response.json())
+       .then((data) => this.displayweather(data));
+    },
+   displayweather: function(data) {
+    const {name} = data;
+    const {icon, description} = data.weather[0];
+    const {temp, humidity} = data.main;
+    const {speed} = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = "weather in " + name;
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + ".png";
+    document.querySelector(".description").innerTaxt = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerTaxt = "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerTaxt = "Wind speed: " + speed + "Km/h";
 
+
+   },
+   search: function () {
+   this.fetchweather(document.querySelector("search-bar").value);
+   }
+};
+document.querySelector(".search button")
+.addEventListener("click", function() {
+weather.search();
+});
